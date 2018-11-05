4b6906a541924c00a0d239c999c7a1900e3e8dcd
Rogério Terciotte <rterciotte@gmail.com>
2018-11-05 18:29:45 -0200

    Obtendo um post por id

---
+++ src/main/java/com/rterciotte/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.rterciotte.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rterciotte.workshopmongo.domain.Post;
+import com.rterciotte.workshopmongo.repository.PostRepository;
+import com.rterciotte.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+
+
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
